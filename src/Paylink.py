2710c4587186a1717c8b33f556f2789bf1d76e29
Antonio Membrides Espinosa <tonykssa@gmail.com>
2021-11-13 23:26:50 -0500

    update create paylink

--- src/Paylink.py
+++ src/Paylink.py
@@ -7,4 +7,8 @@ class Paylink:
         self.sdk = TropiPy.sdk()
 
     def create(self, option):
-        
+        url = self.sdk.getUrl('paylink') 
+        access_token = self.sdk.cfg['token']['access_token']
+        response = requests.post(url, json=option, headers={'Authorization' : 'Bearer {access_token}'})
+        data = response.json()
+        return data
